732981e1c9ab2a1a50d9c8eb1f4d2cce77af4efc
Edward Barber <barber_ed_2@hotmail.com>
2019-01-17 18:02:37 -0500

    Added role column to admin accounts page

--- GroceryStore/Areas/Identity/Pages/Account/Manage/Accounts.cshtml.cs
+++ GroceryStore/Areas/Identity/Pages/Account/Manage/Accounts.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using GroceryStore.Data;
 using GroceryStore.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -13,18 +15,44 @@ namespace GroceryStore.Areas.Identity.Pages.Account.Manage
     [Authorize(Roles = "Admin")]
     public class AccountsModel : PageModel
     {
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public AccountsModel(UserManager<ApplicationUser> userManager)
+        [BindProperty]
+        public OutputModel Output { get; set; }
+
+        public class OutputModel
         {
-            _userManager = userManager;
+            public string Username { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Role { get; set; }
         }
 
-        public List<ApplicationUser> Users { get; set; }
+        public AccountsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<OutputModel> Users { get; set; }
 
         public IActionResult OnGet()
         {
-            Users = _userManager.Users.ToList();
+            Users = (from u in _context.Users
+                    join ur in _context.UserRoles on u.Id equals ur.UserId into urGroup
+                    from urItem in urGroup.DefaultIfEmpty()
+                    join r in _context.Roles on urItem.RoleId equals r.Id into rGroup
+                    from rItem in rGroup.DefaultIfEmpty()
+                    select new OutputModel
+                    {
+                        Username = u.UserName,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        Email = u.Email,
+                        PhoneNumber = u.PhoneNumber ?? "N/A",
+                        Role = rItem.Name ?? "N/A"
+                    }).ToList();
 
             return Page();
         }
