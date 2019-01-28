f9347a5cb5766d30ae1d0724c1310d57844670b2
Edward Barber <barber_ed_2@hotmail.com>
2019-01-28 16:07:20 -0500

    Added create and delete accounts to admin account page.

--- GroceryStore/Areas/Identity/Pages/Account/Manage/Accounts.cshtml.cs
+++ GroceryStore/Areas/Identity/Pages/Account/Manage/Accounts.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace GroceryStore.Areas.Identity.Pages.Account.Manage
 {
@@ -16,20 +17,32 @@ namespace GroceryStore.Areas.Identity.Pages.Account.Manage
     public class AccountsModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<AccountsModel> _logger;
+
+        [TempData]
+        public string StatusMessage { get; set; }
 
         public class OutputModel
         {
+
             public string Username { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string Email { get; set; }
             public string PhoneNumber { get; set; }
             public string Role { get; set; }
+            public string Id { get; set; }
+            public bool IdSelected { get; set; }
         }
 
-        public AccountsModel(ApplicationDbContext context)
+        public AccountsModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<AccountsModel> logger, SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _logger = logger;
+            _signInManager = signInManager;
         }
 
         public List<OutputModel> Users { get; set; }
@@ -48,10 +61,31 @@ namespace GroceryStore.Areas.Identity.Pages.Account.Manage
                         LastName = u.LastName,
                         Email = u.Email,
                         PhoneNumber = u.PhoneNumber ?? "N/A",
-                        Role = rItem.Name ?? "N/A"
+                        Role = rItem.Name ?? "N/A",
+                        Id = u.Id
                     }).ToList();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                StatusMessage = $"Unable to load user with ID '{id}'.";
+                return new JsonResult(string.Empty);
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                StatusMessage = $"Unexpected error occurred deleteing user with ID '{id}'.";
+                return new JsonResult(string.Empty);
+            }
+
+            _logger.LogInformation($"User with ID '{id}' deleted by admin.");
+            return new JsonResult(string.Empty);
         }
     }
 }
---
+++ GroceryStore/Areas/Identity/Pages/Account/Manage/AddAccount.cshtml.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using GroceryStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GroceryStore.Areas.Identity.Pages.Account.Manage
+{
+    [Authorize(Roles = "Admin")]
+    public class AddAccountModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly ILogger<AddAccountModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public AddAccountModel(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, ILogger<AddAccountModel> logger, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Display(Name = "Phone number")]
+            [Phone]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.UserName,
+                    Email = Input.Email,
+                    PhoneNumber = Input.PhoneNumber,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                List<IdentityError> errors = new List<IdentityError>();
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Admin created a new account with password.");
+
+                    result = await _userManager.AddToRoleAsync(user, "User");
+
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User role added to user account.");
+                    }
+                    else
+                    {
+                        errors.AddRange(result.Errors);
+                    }
+                }
+                else
+                {
+                    errors.AddRange(result.Errors);
+                }
+
+                if (errors.Count == 0)
+                {
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    return Redirect("./Accounts");
+                }
+
+                foreach (var error in errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
