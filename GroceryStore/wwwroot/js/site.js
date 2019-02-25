9b422b6926d13658fa18b0b9bd2a794264ea0f3e
Edward Barber <barber_ed_2@hotmail.com>
2019-02-25 15:04:32 -0500

    Details overlay over image on Groceries page

--- GroceryStore/wwwroot/js/site.js
+++ GroceryStore/wwwroot/js/site.js
@@ -2,3 +2,62 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Internet Explorer 
+var isIE = navigator.userAgent.indexOf("MSIE") != -1 || (!!document.documentMode == true);
+
+// call coresponding functions here
+// ------------------------------------------------
+$(document).ready(function () {
+    $('.selectable-image-info').each(function (index, element) {
+        if (isIE) {
+            centerSelectableImageInfo(element);
+        }
+
+        // add hidden attribute instead of on page itself because elements' width don't
+        // get calculated when they are hidden
+        $(element).addClass('hidden');
+    });
+
+    // use current target as it should pass in the correct element
+    $('.selectable-image').on({
+        mouseenter: function (event) {
+            toggleSelectableImage(event.currentTarget);
+        },
+        mouseleave: function (event) {
+            toggleSelectableImage(event.currentTarget);
+        }
+    });
+});
+// ------------------------------------------------
+
+// seperate functionality here
+// ---------------------------------------------------------
+
+// odd way of centering an element however this is the only "hack" (margin 0 auto doesn't work correctly on page) way it will work in IE 11 as
+// flex boxes aren't fully compatible
+function centerSelectableImageInfo(element) {
+    var container = $(element).parent();
+
+    var elementWidth = $(element).outerWidth();
+    var containerWidth = container.width();
+
+    var marginLeft = (containerWidth / 2 - elementWidth / 2) / containerWidth * 100;
+
+    $(element).css('margin-left', marginLeft.toString() + '%');
+}
+
+function toggleSelectableImage(element) {
+    var image = $(element).children('img').first();
+    var info = $(element).children('.selectable-image-info').first();
+
+    if (image.css('opacity') == undefined || image.css('opacity') > 0.5) {
+        image.css('opacity', 0.5);
+        info.removeClass('hidden');
+    }
+    else {
+        image.css('opacity', 1);
+        info.addClass('hidden');
+    }
+}
+// ---------------------------------------------------------
